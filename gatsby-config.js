b5c83ed2c94b183016ad27503cde90cf2a947e24
Josiah Eakle <josiaheakle@gmail.com>
2021-10-31 00:54:02 -0400

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,13 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ],
 };
